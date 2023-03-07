7d5fa383b8579d2003cab4286dfc80148f07bf7d
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-03-07 12:20:31 +0000

    added index, update and delete actions

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,15 +1,36 @@
 class ArtworksController < ApplicationController
+  def index
+    @artworks = Artwork.all
+  end
+
   def new
     @artwork = Artwork.new
   end
 
   def create
     artwork = Artwork.new(artwork_params)
+    artwork.save
     redirect_to artwork_path(artwork)
   end
 
   def show
     @artwork = Artwork.find(params[:id])
+  end
+
+  def edit
+    @artwork = Artwork.find(params[:id])
+  end
+
+  def update
+    artwork = Artwork.find(params[:id])
+    artwork.update(artwork_params)
+    redirect_to artwork_path(artwork)
+  end
+
+  def delete
+    @artwork = Artwork.find(params[:id])
+    @artwork.destroy
+    redirect_to artworks_path, status: see_other
   end
 
   private
