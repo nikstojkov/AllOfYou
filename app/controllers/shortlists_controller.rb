047afceb349225bbfecf4ae1a754265866d8c572
Ariana Nelson <arinola540@gmail.com>
2023-03-07 17:22:37 +0000

    made shortlist controller

---
+++ app/controllers/shortlists_controller.rb
@@ -0,0 +1,43 @@
+class ShortlistsController < ApplicationController
+  before_action :set_shortlist, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def index
+    @shortlists = Shortlist.all
+  end
+
+  def new
+    @shortlist = Shortlist.new
+  end
+
+  def create
+    @shortlist = Shortlist.new(shortlist_params)
+    @shortlist.save
+    redirect_to shortlist_path(@shortlist)
+  end
+
+  def edit
+  end
+
+  def update
+    @shortlist.update(shortlist_params)
+    redirect_to shortlist_path(@shortlist)
+  end
+
+  def destroy
+    @shortlist.destroy
+    redirect_to shortlists_path
+  end
+
+  private
+
+  def set_shortlist
+    @shortlist = Shortlist.find(params[:id])
+  end
+
+  def shortlist_params
+    params.require(:shortlist).permit(:name)
+  end
+end
