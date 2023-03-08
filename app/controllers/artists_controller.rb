f72bf53e00111750ba5968d4bc9d168652ff2200
Nik Stojkov <nik.stojkov@gmail.com>
2023-03-08 21:28:09 +0000

    can return one artist per single tag

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,8 +2,17 @@ class ArtistsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
   before_action :set_artist, only: %i[show update edit destroy]
 
+
   def index
-    @artists = Artist.all
+    @artist_tags = ArtistTag.all
+
+    if params[:query].present?
+      # @artists = Artist.where(title: params[:query])
+      @artists = Artist.joins(:tags).where(tags: { name: params[:query] })
+
+    else
+      @artists = Artist.all
+    end
   end
 
   def show
