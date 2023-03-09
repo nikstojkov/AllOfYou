9fdae2c82da0bad2b941a0e7bf3204e5de317510
Nik Stojkov <nik.stojkov@gmail.com>
2023-03-09 16:09:55 +0000

    added search route

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,15 @@
+class SearchController < ApplicationController
+
+  def index
+    if params[:query].present?
+      tagsquery = params[:query].split
+      @artists = Artist.joins(:tags).where(tags: {name: tagsquery} )
+      @artworks = Artwork.joins(:tags).where(tags: {name: tagsquery} )
+      @opportunities = Opportunity.joins(:tags).where(tags: {name: tagsquery} )
+    else
+      @artists = Artist.all
+      @artworks = Artwork.all
+      @opportunities = Opportunity.all
+    end
+  end
+end
