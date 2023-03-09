50a92504cc0389c91b9bc0e5e507b20b623c6004
Nik Stojkov <nik.stojkov@gmail.com>
2023-03-09 19:46:22 +0000

    fixed user/artist validation errors

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :shortlists
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :artists
-
   resources :users
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :artworks, except: :show
-
-  resources :opportunities do
-    resources :applications, except: :edit
-  end
+  resources :opportunities
+  resources :applications, except: :edit
 end
