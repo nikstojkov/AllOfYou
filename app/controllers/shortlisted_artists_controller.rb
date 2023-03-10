884aa43dfd958c6d9ceb154ac7751cfcf69bf59b
Nik Stojkov <nik.stojkov@gmail.com>
2023-03-10 15:15:23 +0000

    adds to shortlist if you have one

--- app/controllers/shortlisted_artists_controller.rb
+++ app/controllers/shortlisted_artists_controller.rb
@@ -1,13 +1,12 @@
 class ShortlistedArtistsController < ApplicationController
 
   def create
-
     @shortlisted_artists = ShortlistedArtist.new
     @artist = Artist.find(params[:artist_id])
     @shortlisted_artists.artist = @artist
     @shortlisted_artists.shortlist = current_user.shortlists.first
     if @shortlisted_artists.save
-      redirect_to root_path
+      redirect_to shortlists_path(current_user)
     else
       redirect_to artist_path(@artist)
     end
