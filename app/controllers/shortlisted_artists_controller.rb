0e7801d7e08b20427be884b9b18d4178be61ba5e
Joel Savage <joel28169@gmail.com>
2023-03-13 12:34:48 +0000

    added new auths and fixed mutli search

--- app/controllers/shortlisted_artists_controller.rb
+++ app/controllers/shortlisted_artists_controller.rb
@@ -1,4 +1,5 @@
 class ShortlistedArtistsController < ApplicationController
+  skip_before_action :authenticate_artist!
 
   def create
     @shortlisted_artists = ShortlistedArtist.new
--- app/controllers/shortlists_controller.rb
+++ app/controllers/shortlists_controller.rb
@@ -1,4 +1,5 @@
 class ShortlistsController < ApplicationController
+  skip_before_action :authenticate_artist!
 
   before_action :set_shortlist, only: %i[show edit update destroy]
 
