3ad58661d5ce7f843996ee962266ec5d1c156245
Nik Stojkov <nik.stojkov@gmail.com>
2023-03-14 17:44:03 +0000

    back button outside of pointless if statement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # after_update :redirect_after_update, if: :devise_controller?, only: [:update]
+
+  private
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
@@ -12,4 +16,8 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :location, :bio, :pronouns])
   end
+
+  # def redirect_after_update
+  #   redirect_to(artists_path) and return
+  # end
 end
