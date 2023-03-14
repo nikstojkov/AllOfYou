0a09dae86bb31c0f34ee37ffbe8d8f315eec1099
Janett Gerrard <mikanchik@gmail.com>
2023-03-14 11:58:22 +0000

    removed console logs, added comments so I can still understand it later

--- app/javascript/lazyloader.js
+++ app/javascript/lazyloader.js
@@ -1,25 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // selecting all the images
   const lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
 
   if ("IntersectionObserver" in window) {
-    console.log("the intersection observer is in the window")
+    // we create the observer with a callback that will be invoked whenever a target entry
+    // crosses the threshold (default when 1 pixel is visible in viewport)
+
     let lazyImageObserver = new IntersectionObserver((entries, observer) => {
+      // entries is the set of targets that have changed their intersection status
       entries.forEach((entry) => {
+        // .isIntersecting returns true if the entry has gone from non-intersecting to intersecting
         if (entry.isIntersecting) {
-          console.log("the entry is intersecting");
+          // if it is intersecting we select the image element
           let lazyImage = entry.target;
-
-          console.log(lazyImage);
-
+          // replace loading gif with actual image src from data-src attribute
           lazyImage.src = lazyImage.dataset.src;
           //lazyImage.srcset = lazyImage.dataset.srcset;
+
+          // remove .lazy class
           lazyImage.classList.remove("lazy");
           lazyImage.classList.add("lazy__loaded");
+          // remove image from set of entries now it is loaded
           lazyImageObserver.unobserve(lazyImage);
         }
       });
     });
 
+    // add each image to the set of targets being watched by LazyImageObserver
     lazyImages.forEach((lazyImage) => {
       lazyImageObserver.observe(lazyImage);
     });
